489ab0b20b96a3c4540c11b297be8fb844efe3a9
Eduardo Fernandes de Magalhaes Pimenta <ed83isme@gmail.com>
2025-03-22 02:52:26 +0000

    tidy up the form. Fixed the form data behavior.

--- scripts/form_submit.js
+++ scripts/form_submit.js
@@ -3,5 +3,11 @@ const gruForm = document.querySelector("#gruForm"); // Get your form element
 gruForm.addEventListener("submit", function (e) {
   // Your custom logic here
   e.preventDefault(); // Prevent the default form submission behavior
-  console.log(new FormData().getAll());
+
+  const formData = new FormData(gruForm);
+  console.log(formData);
+  // Itera sobre os dados do formulário
+  for (let [field, value] of formData.entries()) {
+    console.log(field + ": " + value);
+  }
 });
